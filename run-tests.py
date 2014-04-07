5ea85d9296854bbd6757052afc6fb86ec36fd845
vic <git@dfhu.org>
2014-04-07 07:02:02 -0400

    add mitm-httpd-proxy as a submodule and see how travis-ci handles this

--- run-tests.py
+++ run-tests.py
@@ -3,4 +3,5 @@ from subprocess import call
 errs = 0
 errs += call(["python", "tests/selenium/static-proxied.py"])
 errs += call(["python", "tests/jasmine/jasmine-runner.py"])
+errs += call(["python", "tests/selenium/httpd-proxied.py"])
 exit(errs)
---
+++ tests/selenium/httpd-proxied.py
@@ -0,0 +1,26 @@
+import sys
+import os
+from os.path import dirname, realpath
+project_base = dirname(dirname(dirname(realpath(__file__))))
+sys.path.append(project_base + "/external/mitm-http-proxy")
+
+from MitmHttpProxy import MitmHttpProxy, Httpd, shutdown_thread
+
+if __name__ == '__main__':
+    os.chdir(project_base + "/src")
+
+    inport = "8777"
+    http_port = "8000"
+
+    httpd = Httpd()
+    httpd.start()
+
+    cap = MitmHttpProxy(
+        '127.0.0.1', int(inport),
+        '127.0.0.1', int(http_port))
+
+    shutdown_thread(cap)
+    shutdown_thread(httpd)
+
+
+    pass
