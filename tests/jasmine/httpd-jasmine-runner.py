024331cc19368288909df994f8b00404c96515f6
vic <git@dfhu.org>
2014-04-13 13:34:33 -0400

    running the jasmin runner through httpd

---
+++ tests/jasmine/httpd-jasmine-runner.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from os.path import dirname, realpath
+project_base = dirname(dirname(dirname(realpath(__file__))))
+sys.path.append(project_base + "/external/mitm-http-proxy")
+
+from time import sleep
+
+from selenium import webdriver
+
+from MitmHttpProxy import MitmHttpProxy, Httpd, shutdown_thread
+
+if __name__ == '__main__':
+    os.chdir(dirname(realpath(__file__)) + "/lib")
+
+    http_port = "8000"
+
+    httpd = Httpd()
+    httpd.start()
+
+    driver = webdriver.Firefox()
+    driver.get("http://127.0.0.1:" + http_port + "/SpecCouchdb.html")
+    driver.close()
+
+    shutdown_thread(httpd)
